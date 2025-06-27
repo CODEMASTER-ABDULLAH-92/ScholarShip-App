ecbce16660f2eec6d4a7de51660d90cf195b28df
CODEMASTER-ABDULLAH-92 <abdullahpeerzada92@gmail.com>
2025-06-27 15:32:30 +0500

    Update ContextApi.jsx

--- client/src/Context/ContextApi.jsx
+++ client/src/Context/ContextApi.jsx
@@ -7,7 +7,7 @@ import { useParams } from "react-router-dom";
 export const ContextApi = createContext();
 
 const ContextProvider = (props) => {
-  const url = "http://localhost:8000";
+  const url = "https://scholar-ship-app-backend-kvt4t23jw.vercel.app";
 
   const [data, setData] = useState([]);
   //User FullDetails Data
